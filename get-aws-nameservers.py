b661290f0468c5eec0060abf91b27aa682c579e0
Juliet Hollerauer <juliet.hollerauer@sturdynetworks.com>
2017-03-24 06:50:59 -0700

    added indent

--- get-aws-nameservers.py
+++ get-aws-nameservers.py
@@ -11,25 +11,25 @@ import os
 
 def main():
 
-client = boto3.client('route53')
-hosted_zones = client.list_hosted_zones()
-zones = []
+    client = boto3.client('route53')
+    hosted_zones = client.list_hosted_zones()
+    zones = []
 
-for ZoneId in hosted_zones.get("HostedZones"):
-    zones.extend([ZoneId.get("Id").translate(None, '/hostedzone/')])
+    for ZoneId in hosted_zones.get("HostedZones"):
+        zones.extend([ZoneId.get("Id").translate(None, '/hostedzone/')])
 
-    for zone in zones:
-        response = client.get_hosted_zone(Id=zone)
-        HostedZone = response.get("HostedZone")
-        DelegationSet = response.get("DelegationSet")
-        if "HostedZone" in response:
-            name = HostedZone.get("Name")
-            zoneid = HostedZone.get("Id").translate(None, '/hostedzone/')
-        if "DelegationSet" in response:
-            nameservers = DelegationSet.get("NameServers", "none")
-        else:
-            nameservers = "None"
-        print(name.replace("'", "") + " " + str(nameservers).strip('[]')).replace("'", "")
+        for zone in zones:
+            response = client.get_hosted_zone(Id=zone)
+            HostedZone = response.get("HostedZone")
+            DelegationSet = response.get("DelegationSet")
+            if "HostedZone" in response:
+                name = HostedZone.get("Name")
+                zoneid = HostedZone.get("Id").translate(None, '/hostedzone/')
+            if "DelegationSet" in response:
+                nameservers = DelegationSet.get("NameServers", "none")
+            else:
+                nameservers = "None"
+            print(name.replace("'", "") + " " + str(nameservers).strip('[]')).replace("'", "")
 
 if __name__ == "__main__":
     main()
